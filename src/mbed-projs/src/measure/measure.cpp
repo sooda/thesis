cc35ac3830b6ae27b125fbe65a7e37771cffc113
Konsta 'sooda' Hölttä <ksooda@gmail.com>
2014-05-27 13:07:44 +0300

    src: shutter lag measurement
    hot shoe connects the middle pin to ground. use pull-up resistor and
    wait for falling edge, timing the delay between shutter release button
    and flash.
    
    eos 700D: ~76 ms for full f/1.8 aperture, same for f/10, ~79 ms for f/22

---
+++ src/mbed-projs/src/measure/measure.cpp
@@ -0,0 +1,49 @@
+#include "mbed.h"
+
+DigitalOut myled(LED1);
+DigitalIn button(USER_BUTTON);
+
+// topmost one
+DigitalOut focus(PC_9);
+DigitalOut shutter(PB_8);
+
+// lowest in column 2
+InterruptIn hotshoe(PC_4);
+
+// "button" down, button up
+// up = release
+int downtime = 0;
+int uptime = 0;
+
+Timer timer;
+
+void feedback() {
+	uptime = timer.read_us();
+}
+
+int main() {
+	printf("o hai\r\n");
+	focus = 0;
+	shutter = 0;
+	myled = 0;
+	hotshoe.fall(&feedback);
+	timer.start();
+
+	for (;;) {
+		while (button != 0)
+			;
+		printf("start\r\n");
+		myled = 1;
+		focus = 1;
+		wait(0.2);
+		downtime = timer.read_us();
+		shutter = 1;
+
+		wait(0.2);
+		shutter = 0;
+		focus = 0;
+
+		int totaltime = uptime - downtime;
+		printf("end %d\r\n", totaltime);
+	}
+}
