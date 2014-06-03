454b7e6284e4834fabecbae5a6511ef74421dcca
Konsta 'sooda' Hölttä <ksooda@gmail.com>
2014-06-03 17:48:19 +0300

    src: tether: capture by shot number, not cam name
    because it's easier to group pics like this

---
+++ src/gphoto-tether/capture-by-shotid.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+if [ "$1" = "" ]; then
+	echo "giev pics dir name"
+	exit 1
+fi
+
+#trap ctrlc INT
+
+function ctrlc() {
+	echo lol
+}
+
+SHOTNAME=$1
+cwd=`pwd`
+
+i=0
+for port in `./cams.sh`; do
+	gphoto="gphoto2 --quiet --port $port"
+	export CAMERAID=`$gphoto --get-config artist|grep ^Current|sed 's/^Current: //'`
+	echo $i:$port:$CAMERAID
+	$gphoto --filename pics/by-shot/$SHOTNAME/%02n/snap-$CAMERAID.%C \
+		--wait-event-and-download \
+		--hook-script $cwd/camhook.sh \
+		| egrep -v "^(UNKNOWN PTP|UNKNOWN Camera)" &
+	i=$((i+1))
+done
+
+wait
