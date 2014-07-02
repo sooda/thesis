44824d9f407cb17d2047dfbdc2e77795729820ed
Konsta 'sooda' Hölttä <ksooda@gmail.com>
2014-07-02 15:33:31 +0300

    tether: rawprocess properly in parallel
    xargs -n1 -P4 is sweet

--- src/gphoto-tether/rawprocess.sh
+++ src/gphoto-tether/rawprocess.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# note: don't worry about "cannot open file *.cr2" or something
-# means just that no pics of that name exist
-
 mkdir -p processed
 
-for i in *.cr2 *.CR2; do
-	ufraw-batch \
-		--wb=camera \
-		--exposure=auto \
-		--out-type=jpeg \
-		--compression=100 \
-		--out-path processed \
-		--size 2000x2000 \
-		$i &
-done
+ls *.cr2 *.CR2 | xargs -n1 -P4 ufraw-batch \
+	--wb=camera \
+	--exposure=auto \
+	--out-type=jpeg \
+	--compression=100 \
+	--out-path processed \
+	--size 2000x2000 \
+	--overwrite
