ee9fe521575cd633de6b270a249595c7d98571b5
Konsta 'sooda' Hölttä <ksooda@gmail.com>
2014-07-04 17:18:53 +0300

    tether: let ufraw-batch handle parallelism
    ufraw-batch actually runs the conversion on all cores, so don't xargs

--- src/gphoto-tether/rawprocess.sh
+++ src/gphoto-tether/rawprocess.sh
@@ -2,11 +2,12 @@
 
 mkdir -p processed
 
-ls *.cr2 *.CR2 | xargs -n1 -P4 ufraw-batch \
+ufraw-batch \
 	--wb=camera \
 	--exposure=auto \
 	--out-type=jpeg \
 	--compression=100 \
 	--out-path processed \
 	--size 2000x2000 \
-	--overwrite
+	--overwrite \
+	*.cr2 *.CR2
