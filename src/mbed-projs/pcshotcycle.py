b670f4e8e2510c3ebb99f2c62e026a9936bd5a10
Konsta 'sooda' Hölttä <ksooda@gmail.com>
2014-07-23 12:35:52 +0300

    mbed: pcshot: add inter-shot delay configuration
    an additional delay when the last camera has finished before the first
    starts the next round again

--- src/mbed-projs/pcshotcycle.py
+++ src/mbed-projs/pcshotcycle.py
@@ -3,17 +3,19 @@ from pcshot import CameraControl, allcams, onlycam, NCAMS
 import sys
 import time
 
-# for continuous mode shooting (i.e. shoot as long as shutter is down), only start shooting separately; next iterations modify nothing, they just wait
+# for continuous mode shooting (i.e. shoot as long as shutter is down), only
+# start shooting separately; next iterations modify nothing, they just wait
 cams_continuous = False
 
 def main():
     count = int(sys.argv[1])
     delay = float(sys.argv[2])
+    delay2 = float(sys.argv[3])
 
     ctl = CameraControl("/dev/ttynucleo0")
 
     ctl.focus(allcams())
-    time.sleep(0.1)
+    time.sleep(3.0)
     state = [False] * 9
     for i in range(count):
         for ci in range(NCAMS):
@@ -23,6 +25,7 @@ def main():
             else:
                 ctl.shoot(onlycam(ci))
             time.sleep(delay)
+        time.sleep(delay2 - delay)
     ctl.reset()
 
 if __name__ == "__main__":
