100b24359c847647224b8228a67b6619a91fa614
Konsta 'sooda' Hölttä <ksooda@gmail.com>
2014-07-24 12:47:27 +0300

    tether: ignore aeb because not working with flash
    - with pop-up flash up, the camera refuses to touch the aeb config
    - also implement error checking for readconfig.sh when getting some
      configs fails

--- src/gphoto-tether/config.sh
+++ src/gphoto-tether/config.sh
@@ -14,14 +14,16 @@ else
 	./forallp.sh \
 		--set-config imageformat=RAW \
 		--set-config imageformatsd=RAW \
-		--set-config shutterspeed="1/10" \
-		--set-config iso="400" \
+		--set-config shutterspeed="1/100" \
+		--set-config iso="100" \
 		--set-config colorspace="sRGB" \
 		--set-config picturestyle="Standard" \
 		--set-config focusmode="One Shot" \
 		--set-config aperture="11" \
 		--set-config meteringmode="Evaluative" \
-		--set-config aeb="off" \
 		--set-config drivemode="Single" \
 		--set-config capturetarget="Memory card"
+
+#		--set-config aeb="off" \
+# aeb can be configured only when the flash is off
 fi
--- src/gphoto-tether/readconfig.sh
+++ src/gphoto-tether/readconfig.sh
@@ -25,7 +25,7 @@ confs=`grep ^/ interesting-settings.txt|sed 's#.*/##'|tr '\n' ' '`
 # prefix each config path with --get-config
 confsget="--get-config ${confs// / --get-config }"
 
-gphoto2 --port $port $confsget| egrep '^(Current|Label)' > currentconfig.txt # sed 'N;s/\n/ /'
+gphoto2 --port $port $confsget | egrep '^(Current|Label)' > currentconfig.txt # sed 'N;s/\n/ /'
 # force this too if it's stuck in internal ram
 echo "Label: Capture Target" >> currentconfig.txt
 echo "Current: Memory card" >> currentconfig.txt
@@ -39,9 +39,14 @@ while read labelline; do
 	read confline
 	labelline=${labelline/Label: /}
 	confline=${confline/Current: /}
+	if [[ "$confline" = Label:* ]]; then
+		echo Bad config, some expected setting could not be read from the camera. >&2
+		echo Check interesting-settings.txt and/or run this script with -x. >&2
+		exit 1 # exits the () loop
+	fi
 	echo "\\"
 	echo -n "    --set-config '$labelline'='$confline' "
-done < currentconfig.txt ; echo) > calibsettings.sh
+done < currentconfig.txt ; echo) > calibsettings.sh || exit 1
 
 chmod +x calibsettings.sh
 cat calibsettings.sh
